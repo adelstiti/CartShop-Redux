b52185a33ee5b30925886ebe150ced8976657a55
adelstiti <adelw126@gmail.com>
2020-09-05 22:18:26 +0100

    Updates

---
+++ precache-manifest.e9f0da4561a798336b1e3b470528b839.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8d62b2c0db0cdd3b0e2a5158843544",
+    "url": "/CartShop-Redux/index.html"
+  },
+  {
+    "revision": "4c4762ad567ca1c961cf",
+    "url": "/CartShop-Redux/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "80fd9c7be6369fe7f12c",
+    "url": "/CartShop-Redux/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4c4762ad567ca1c961cf",
+    "url": "/CartShop-Redux/static/js/2.d63d6b76.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/CartShop-Redux/static/js/2.d63d6b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80fd9c7be6369fe7f12c",
+    "url": "/CartShop-Redux/static/js/main.e5760abb.chunk.js"
+  },
+  {
+    "revision": "34b26ef19d90df8b07e5",
+    "url": "/CartShop-Redux/static/js/runtime-main.804e5c2c.js"
+  }
+]);
